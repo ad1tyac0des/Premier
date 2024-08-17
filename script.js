f9c5575eb143310fdd1cb002455f086d738b33ad
Aditya Singh <adityasingh740745@gmail.com>
2024-08-17 14:00:28 +0530

    feat: add pg 2 full

---
+++ script.js
@@ -0,0 +1,20 @@
+function createCircularText() {
+    const paraContent =
+        "Follow me and be in touch  Interested in quality design? ";
+    const para = document.querySelector("#circ-text");
+
+    window.onload = () => {
+        for (let i = 0; i < paraContent.length; i++) {
+            const span = document.createElement("span");
+            span.innerText = paraContent[i];
+            para.appendChild(span);
+            span.style.transformOrigin = `0 ${para.offsetWidth / 2}px`;
+            console.log(para.offsetWidth / 2);
+            span.style.rotate = `${(360 / paraContent.length) * i}deg`;
+        }
+    };
+
+    console.log(para);
+}
+
+createCircularText()
